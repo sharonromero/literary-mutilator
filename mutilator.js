7fa206e0854cfc1a5cf731095bed4197833c812e
sharonromero <sh-aron@hotmail.com>
2018-11-18 22:15:03 -0700

    Tried more functions that don't work

--- mutilator.js
+++ mutilator.js
@@ -5,4 +5,27 @@
 
 //document.getElementById("textColor").style.fontFamily = "Impact,Charcoal,sans-serif";
 
-document.getElementById("textColor").style.color = "blue";
+//function changeText() {
+	//document.getElementById("textColor").style.color = "blue";
+//}
+
+
+//var pickWord = function(word) {
+	//return word[shizzle];
+
+//};
+
+//var paragraph = document.getElementById("textColor");
+//var newParagraphText = prompt("change words");
+//paragraph.innerHTML = newParagraphText;
+
+//function myFunction() {
+	//document.getElementById("color").innerHTML.style.font = "italic bold 20px";
+//}
+
+//function myFunction() {
+	//document.getElementById('color').innerHTML = 'Shizzle';
+
+document.getElementById("color").innerHTML = "Replace";
+
+//}
