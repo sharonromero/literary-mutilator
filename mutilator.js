2b2d65b2a93aff28bafad6f849774a795bff83b0
sharonromero <sh-aron@hotmail.com>
2018-11-18 21:38:36 -0700

    Added various functions that don't work

---
+++ mutilator.js
@@ -0,0 +1,8 @@
+//var paragraph = document.getElementById("textColor");
+//paragraph.setAttribute("p", "this sucks"); {
+
+//}
+
+//document.getElementById("textColor").style.fontFamily = "Impact,Charcoal,sans-serif";
+
+document.getElementById("textColor").style.color = "blue";
