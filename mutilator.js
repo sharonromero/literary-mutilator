34649a8487614b35c603d5e881e4ccb7cd9d1bc4
sharonromero <sh-aron@hotmail.com>
2018-11-18 22:37:30 -0700

    Added another function that doesn't work

--- mutilator.js
+++ mutilator.js
@@ -1,33 +1,33 @@
-//var paragraph = document.getElementById("textColor");
-//paragraph.setAttribute("p", "this sucks"); {
+var paragraph = document.getElementById("textColor");
+paragraph.setAttribute("p", "this sucks"); {
 
-//}
+}
 
-//document.getElementById("textColor").style.fontFamily = "Impact,Charcoal,sans-serif";
+document.getElementById("textColor").style.fontFamily = "Impact,Charcoal,sans-serif";
 
-//function changeText() {
-	//document.getElementById("textColor").style.color = "blue";
-//}
+function changeText() {
+	document.getElementById("textColor").style.color = "blue";
+}
 
 
-//var pickWord = function(word) {
-	//return word[shizzle];
+var pickWord = function(word) {
+	return word[shizzle];
 
-//};
+}
 
-//var paragraph = document.getElementById("textColor");
-//var newParagraphText = prompt("change words");
-//paragraph.innerHTML = newParagraphText;
+var paragraph = document.getElementById("textColor");
+var newParagraphText = prompt("change words");
+paragraph.innerHTML = newParagraphText;
 
-//function myFunction() {
-	//document.getElementById("color").innerHTML.style.font = "italic bold 20px";
-//}
+function myFunction() {
+	document.getElementById("color").innerHTML.style.font = "italic bold 20px";
+}
 
-//function myFunction() {
-	//document.getElementById('color').innerHTML = 'Shizzle';
+function myFunction() {
+	document.getElementById('color').innerHTML = 'Shizzle';
 
-//document.getElementById("color").innerHTML = "Replace";
-//}
+document.getElementById("color").innerHTML = "Replace";
+}
 
 function changeText() {
 	document.getElementById("textChanger").onclick = function() {
